39363110a28fe691965ee2a52f194d2770ce4879
Tejas Farera <tejas@Tejass-MacBook-Air.local>
2021-09-11 18:01:19 +0530

    test yml

--- GuessTheFlag/SceneDelegate.swift
+++ GuessTheFlag/SceneDelegate.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow? 
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
